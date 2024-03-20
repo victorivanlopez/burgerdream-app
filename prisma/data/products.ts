0eb4aa6a4c7b700bfe408c03e145859c6cf835e9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-20 15:18:31 -0600

    Created: products data

---
+++ prisma/data/products.ts
@@ -0,0 +1,152 @@
+export const products = [
+  {
+    name: "Hamburguesa clásica",
+    price: 8.99,
+    image: "hamburguesa_clasica",
+    categoryId: 1, // ID de la categoría de hamburguesas
+  },
+  {
+    name: "Alitas picantes",
+    price: 10.49,
+    image: "alitas_picantes",
+    categoryId: 2, // ID de la categoría de alitas
+  },
+  {
+    name: "Sandwich de pollo",
+    price: 7.99,
+    image: "sandwich_pollo",
+    categoryId: 3, // ID de la categoría de sandwiches
+  },
+  {
+    name: "Baguette de jamón y queso",
+    price: 6.99,
+    image: "baguette_jamon_queso",
+    categoryId: 4, // ID de la categoría de baguettes
+  },
+  {
+    name: "Refresco de cola",
+    price: 1.99,
+    image: "refresco_cola",
+    categoryId: 5, // ID de la categoría de bebidas
+  },
+  {
+    name: "Hamburguesa vegetariana",
+    price: 9.99,
+    image: "hamburguesa_vegetariana",
+    categoryId: 1, // ID de la categoría de hamburguesas
+  },
+  {
+    name: "Alitas de BBQ",
+    price: 11.99,
+    image: "alitas_bbq",
+    categoryId: 2, // ID de la categoría de alitas
+  },
+  {
+    name: "Sandwich de atún",
+    price: 8.49,
+    image: "sandwich_atun",
+    categoryId: 3, // ID de la categoría de sandwiches
+  },
+  {
+    name: "Baguette de salmón ahumado",
+    price: 9.99,
+    image: "baguette_salmon_ahumado",
+    categoryId: 4, // ID de la categoría de baguettes
+  },
+  {
+    name: "Agua mineral",
+    price: 0.99,
+    image: "agua_mineral",
+    categoryId: 5, // ID de la categoría de bebidas
+  },
+  {
+    name: "Hamburguesa doble queso",
+    price: 11.49,
+    image: "hamburguesa_doble_queso",
+    categoryId: 1, // ID de la categoría de hamburguesas
+  },
+  {
+    name: "Alitas teriyaki",
+    price: 12.49,
+    image: "alitas_teriyaki",
+    categoryId: 2, // ID de la categoría de alitas
+  },
+  {
+    name: "Sandwich de pollo a la parrilla",
+    price: 8.99,
+    image: "sandwich_pollo_parrilla",
+    categoryId: 3, // ID de la categoría de sandwiches
+  },
+  {
+    name: "Baguette de roast beef",
+    price: 7.99,
+    image: "baguette_roast_beef",
+    categoryId: 4, // ID de la categoría de baguettes
+  },
+  {
+    name: "Zumo de naranja",
+    price: 1.49,
+    image: "zumo_naranja",
+    categoryId: 5, // ID de la categoría de bebidas
+  },
+  {
+    name: "Hamburguesa de pollo crispy",
+    price: 10.99,
+    image: "hamburguesa_pollo_crispy",
+    categoryId: 1, // ID de la categoría de hamburguesas
+  },
+  {
+    name: "Alitas picantes estilo coreano",
+    price: 13.99,
+    image: "alitas_picantes_coreanas",
+    categoryId: 2, // ID de la categoría de alitas
+  },
+  {
+    name: "Sandwich de roast beef",
+    price: 9.49,
+    image: "sandwich_roast_beef",
+    categoryId: 3, // ID de la categoría de sandwiches
+  },
+  {
+    name: "Baguette vegetariano",
+    price: 8.49,
+    image: "baguette_vegetariano",
+    categoryId: 4, // ID de la categoría de baguettes
+  },
+  {
+    name: "Cerveza artesanal IPA",
+    price: 4.99,
+    image: "cerveza_ipa",
+    categoryId: 5, // ID de la categoría de bebidas
+  },
+  {
+    name: "Hamburguesa con champiñones",
+    price: 11.49,
+    image: "hamburguesa_champinones",
+    categoryId: 1, // ID de la categoría de hamburguesas
+  },
+  {
+    name: "Alitas a la parrilla",
+    price: 12.99,
+    image: "alitas_parrilla",
+    categoryId: 2, // ID de la categoría de alitas
+  },
+  {
+    name: "Sandwich de pavo y queso",
+    price: 8.99,
+    image: "sandwich_pavo_queso",
+    categoryId: 3, // ID de la categoría de sandwiches
+  },
+  {
+    name: "Baguette de pollo asado",
+    price: 7.49,
+    image: "baguette_pollo_asado",
+    categoryId: 4, // ID de la categoría de baguettes
+  },
+  {
+    name: "Limonada casera",
+    price: 2.49,
+    image: "limonada_casera",
+    categoryId: 5, // ID de la categoría de bebidas
+  },
+]
