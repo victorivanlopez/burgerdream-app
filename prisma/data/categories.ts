ed8d6dd7d5777d3627392708a964652d79224d35
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-20 15:18:12 -0600

    Created: categories data

---
+++ prisma/data/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    slug: "hamburguesas",
+    name: "Hamburguesas",
+  },
+  {
+    slug: "alitas",
+    name: "Alitas",
+  },
+  {
+    slug: "sandwiches",
+    name: "Sandwiches",
+  },
+  {
+    slug: "baguette",
+    name: "Baguette",
+  },
+  {
+    slug: "bebidas",
+    name: "Bebidas",
+  },
+];
