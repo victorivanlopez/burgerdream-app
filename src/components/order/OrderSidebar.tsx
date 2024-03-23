16693c6635e4e625c93da7d0755120d31c52e518
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-22 18:43:50 -0600

    Added: <CategoryList/>

--- src/components/order/OrderSidebar.tsx
+++ src/components/order/OrderSidebar.tsx
@@ -1,6 +1,18 @@
+import { prisma } from '@/libs';
+import { CategoryList } from './category';
 
-export default function OrderSidebar() {
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+
   return (
-    <div>OrderSidebar</div>
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className='mt-10'>
+        <CategoryList categories={categories} />
+      </nav>
+    </aside>
   )
 }
