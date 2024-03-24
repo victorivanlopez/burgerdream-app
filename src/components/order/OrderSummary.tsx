28d8a497aedd95f5f29537f4c6e0a5bde790d6a1
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-23 20:14:39 -0600

    Updated: styles

--- src/components/order/OrderSummary.tsx
+++ src/components/order/OrderSummary.tsx
@@ -1,6 +1,8 @@
 
 export default function OrderSummary() {
   return (
-    <div>OrderSummary</div>
+    <aside className="lg:h-screen lg:overflow-y-auto md:w-64 lg:w-80 p-5 shadow">
+      <h1 className="text-3xl text-center font-black">Mi pedido</h1>
+    </aside>
   )
 }
