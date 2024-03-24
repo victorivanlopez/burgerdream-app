7d70eb6b22fe92e1720c9f4afe13bb8f408f3a3b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-23 23:30:08 -0600

    Updated: styles

--- src/app/order/layout.tsx
+++ src/app/order/layout.tsx
@@ -3,10 +3,10 @@ import { OrderSidebar, OrderSummary } from '@/components/order';
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <>
-      <div className="md:flex">
+      <div className="xl:flex">
         <OrderSidebar />
 
-        <main className='md:flex-1 md:h-screen md:overflow-y-auto p-5'>
+        <main className='xl:h-screen xl:flex-1 xl:overflow-y-auto p-5'>
           {children}
         </main>
 
