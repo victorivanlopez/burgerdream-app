11f6ca20b0f9d778c05ce046bb5cee4f1c6c6562
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-26 14:02:55 -0600

    Added: there is order condition

--- src/components/order/OrderSummary.tsx
+++ src/components/order/OrderSummary.tsx
@@ -1,8 +1,19 @@
+'use client'
+import { useOrderStore } from '@/stores';
 
 export default function OrderSummary() {
+
+  const order = useOrderStore((state) => state.order);
+
   return (
     <aside className="lg:h-screen lg:overflow-y-auto md:w-64 lg:w-80 p-5 bg-white shadow">
       <h1 className="text-3xl text-center font-black">Mi pedido</h1>
+
+      {
+        order.length === 0
+          ? <p className='py-5 text-center'>No hay productos en el pedido.</p>
+          : <p>Se seleccionó algo.</p>
+      }
     </aside>
   )
 }
