c49a75d666669ad19a2c3e0d711ab0d05cb90d5b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-26 13:26:42 -0600

    Added: dynamic styles

--- src/components/order/category/CategoryItem.tsx
+++ src/components/order/category/CategoryItem.tsx
@@ -10,12 +10,12 @@ export default function CategoryItem({ category, currentCategory }: CategoryIcon
   return (
     <Link
       href={`/order/${category.slug}`}
-      className={`${category.slug === currentCategory ? 'bg-orangeburger-400' : ''} flex items-center gap-4 xl:border-t border-gray-200 p-5 xl:p-4 xl:last-of-type:border-b xl:hover:bg-orangeburger-400 transition-colors`}
+      className={`${category.slug === currentCategory ? 'bg-orangeburger-400' : ''} flex items-center justify-center xl:justify-start gap-4 xl:border-t border-gray-200 py-6 px-8 xl:p-4 xl:last-of-type:border-b xl:hover:bg-orangeburger-400 transition-colors rounded-md`}
     >
       <img
         src={`/assets/icon-${category.slug}.png`}
         alt={`Icono ${category.name}`}
-        className='w-8 xl:w-auto'
+        className='w-10 xl:w-auto'
         width={64}
         height={64}
       />
