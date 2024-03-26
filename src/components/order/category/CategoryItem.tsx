70643bdca86c28b047a5a5d6ada73b09b454b8da
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-25 23:45:39 -0600

    Added: currentCategory functionality

--- src/components/order/category/CategoryItem.tsx
+++ src/components/order/category/CategoryItem.tsx
@@ -1,15 +1,16 @@
-import type { Category } from '@prisma/client';
 import Link from 'next/link';
+import type { Category } from '@prisma/client';
 
 interface CategoryIconProps {
   category: Category;
+  currentCategory: string;
 }
 
-export default function CategoryItem({ category }: CategoryIconProps) {
+export default function CategoryItem({ category, currentCategory }: CategoryIconProps) {
   return (
     <Link
       href={`/order/${category.slug}`}
-      className={` flex items-center gap-4 xl:border-t border-gray-200 p-5 xl:p-4 xl:last-of-type:border-b xl:hover:bg-orangeburger-400 transition-colors`}
+      className={`${category.slug === currentCategory ? 'bg-orangeburger-400' : ''} flex items-center gap-4 xl:border-t border-gray-200 p-5 xl:p-4 xl:last-of-type:border-b xl:hover:bg-orangeburger-400 transition-colors`}
     >
       <img
         src={`/assets/icon-${category.slug}.png`}
