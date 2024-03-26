b89c9fb2c37b536d733e0242bc2b41e2f37d2e27
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-25 23:46:02 -0600

    Added: useParams functionality

--- src/components/order/category/CategoryList.tsx
+++ src/components/order/category/CategoryList.tsx
@@ -1,11 +1,16 @@
+'use client'
 import { Category } from '@prisma/client';
 import { CategoryItem } from '.';
+import { useParams } from 'next/navigation';
 
 interface CategoryListProps {
   categories: Category[];
 }
 
 export default function CategoryList({ categories }: CategoryListProps) {
+
+  const params = useParams<{category: string}>();
+  
   return (
     <div className='flex gap-10 overflow-x-auto xl:py-0 xl:block xl:overflow-hidden'>
       {
@@ -13,6 +18,7 @@ export default function CategoryList({ categories }: CategoryListProps) {
           <CategoryItem
             key={category.id}
             category={category}
+            currentCategory={params.category}
           />
         ))
       }
