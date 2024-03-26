dbbb89c4e42478c54b9b615470ff4aaeae68086f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-26 14:59:39 -0600

    Updated: texts

--- src/components/order/OrderSummary.tsx
+++ src/components/order/OrderSummary.tsx
@@ -7,11 +7,11 @@ export default function OrderSummary() {
 
   return (
     <aside className="lg:h-screen lg:overflow-y-auto md:w-64 lg:w-80 p-5 bg-white shadow">
-      <h1 className="text-3xl text-center font-black">Mi pedido</h1>
+      <h1 className="text-3xl text-center font-black">Mi orden</h1>
 
       {
         order.length === 0
-          ? <p className='py-5 text-center'>No hay productos en el pedido.</p>
+          ? <p className='py-5 text-center'>No hay productos aún.</p>
           : <p>Se seleccionó algo.</p>
       }
     </aside>
