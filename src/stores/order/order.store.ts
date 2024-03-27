3b381811125333baa2db3ed857d33e452573cb38
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-26 19:40:28 -0600

    Added: addProductToOrder method

--- src/stores/order/order.store.ts
+++ src/stores/order/order.store.ts
@@ -1,10 +1,34 @@
-import { create } from 'zustand';
+import { StateCreator, create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import type { Product } from '@prisma/client';
 import type { OrderItem } from '@/types';
 
 interface OrderStore {
   order: OrderItem[];
+  addProductToOrder: (product: Product) => void;
 }
 
-export const useOrderStore = create<OrderStore>(() => ({
+const storeAPI: StateCreator<OrderStore, [['zustand/devtools', never]]> = (set) => ({
   order: [],
-}));
+
+  addProductToOrder: (product: Product) => {
+    const { id, name, price } = product;
+    const newProduct = {
+      id,
+      name,
+      price,
+      qty: 1,
+      subtotal: 1 * price,
+    }
+
+    set(state => ({
+      order: [...state.order, newProduct]
+    }), false, 'addProductToOrder');
+  },
+})
+
+export const useOrderStore = create<OrderStore>()(
+  devtools(
+    storeAPI
+  )
+);
