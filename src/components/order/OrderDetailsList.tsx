986c1a3cfc00e1a7e08813b59d56939829ff9ca0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-28 19:15:17 -0600

    Added: <OrderDetailsItem />

--- src/components/order/OrderDetailsList.tsx
+++ src/components/order/OrderDetailsList.tsx
@@ -1,4 +1,5 @@
 import { OrderItem } from '@/types';
+import { OrderDetailsItem } from '.';
 
 interface OrderDetailsListProps {
   order: OrderItem[];
@@ -6,8 +7,14 @@ interface OrderDetailsListProps {
 
 export default function OrderDetailsList({ order }: OrderDetailsListProps) {
   return (
-    <div className='py-5 grid grid-cols-1 gap-2'>
-      OrderDetailList
+    <div className='py-5 grid grid-cols-1'>
+      {
+        order.map(product => (
+          <OrderDetailsItem
+            key={product.id}
+            product={product} />
+        ))
+      }
     </div>
   )
 }
