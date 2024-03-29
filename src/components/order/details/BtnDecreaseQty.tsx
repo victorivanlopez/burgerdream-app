a784f5004c34918dde087887a14b67d7821fe8bc
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-29 11:57:17 -0600

    Created: BtnDecreaseQty component

---
+++ src/components/order/details/BtnDecreaseQty.tsx
@@ -0,0 +1,11 @@
+import { MinusIcon } from '@/components/ui/icons';
+
+export default function BtnDecreaseQty() {
+  return (
+    <button
+      type='button'
+    >
+      <MinusIcon />
+    </button>
+  )
+}
