41e7e2dad12e0eef47c3ea226011b570443bdaf2
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-28 20:09:00 -0600

    Deleted: components

--- src/components/order/index.ts
+++ src/components/order/index.ts
@@ -1,3 +1,2 @@
-export { default as OrderDetailsList } from './OrderDetailsList';
 export { default as OrderSidebar } from './OrderSidebar';
 export { default as OrderSummary } from './OrderSummary';
