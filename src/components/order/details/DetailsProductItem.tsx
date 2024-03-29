6b205831f84e3dd27f9e40741e23810a1b0708df
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-29 11:57:00 -0600

    Created: DetailsProductItem component

---
+++ src/components/order/details/DetailsProductItem.tsx
@@ -0,0 +1,41 @@
+import {
+  BtnAddQty,
+  BtnDecreaseQty,
+  BtnDeleteProductOrder
+} from '.';
+import { formatToDollars } from '@/helpers';
+import type { OrderItem } from '@/types';
+
+interface OrderDetailsItemProps {
+  product: OrderItem;
+}
+
+export default function DetailsProductItem({ product }: OrderDetailsItemProps) {
+  return (
+    <div className='bg-white border p-4'>
+      <div className='flex justify-between'>
+        <h5 className='font-bold'>{product.name}</h5>
+        <span className='text-redburger-500 font-black'>{formatToDollars(product.price)}</span>
+      </div>
+
+      <div className='w-24 p-2 border rounded-full my-4'>
+        <div className='flex justify-between items-center'>
+          {
+            product.qty === 1
+              ? <BtnDeleteProductOrder />
+              : <BtnDecreaseQty />
+          }
+
+          <p>{product.qty}</p>
+
+          <BtnAddQty />
+        </div>
+      </div>
+
+      <div className='flex justify-between text-sm'>
+        <p className='font-bold'>Subtotal:</p>
+        <span>{product.subtotal}</span>
+      </div>
+    </div>
+  )
+}
