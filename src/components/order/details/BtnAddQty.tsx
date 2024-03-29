56d7166f2f6d7d732f9b4074635f1f9d192148ff
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-29 11:57:36 -0600

    Created: BtnAddQty component

---
+++ src/components/order/details/BtnAddQty.tsx
@@ -0,0 +1,11 @@
+import { PlusIcon } from '@/components/ui/icons';
+
+export default function BtnAddQty() {
+  return (
+    <button
+      type='button'
+    >
+      <PlusIcon />
+    </button>
+  )
+}
---
+++ src/components/order/details/index.ts
@@ -0,0 +1,5 @@
+export { default as BtnAddQty } from './BtnAddQty';
+export { default as BtnDecreaseQty } from './BtnDecreaseQty';
+export { default as BtnDeleteProductOrder } from './BtnDeleteProductOrder';
+export { default as DetailsProductItem } from './DetailsProductItem';
+export { default as DetailsProductList } from './DetailsProductList';
