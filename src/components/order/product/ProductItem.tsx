5a2af072257668dce8769d1508b279529f6c27d4
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-30 21:08:02 -0600

    Added: <ButtonProduct />

--- src/components/order/product/ProductItem.tsx
+++ src/components/order/product/ProductItem.tsx
@@ -1,6 +1,6 @@
+import { ButtonProduct } from '.';
 import { formatToDollars } from '@/helpers';
 import type { Product } from '@prisma/client';
-import { ButtonAddProduct } from '.';
 
 interface ProductItemProps {
   product: Product;
@@ -12,12 +12,11 @@ export default function ProductItem({ product }: ProductItemProps) {
     <div className=" bg-white border border-gray-200 rounded-lg shadow md:flex-row">
       <img className="object-cover rounded-t-lg h-48 w-full md:rounded-none md:rounded-s-lg" src={`/assets/products/${product.image}.webp`} alt={`Imagenen del producto ${product.name}`} />
       <div className="p-4">
-        <h5 className="text-2xl font-bold mb-5">{product.name}</h5>
-        <div className='flex items-center justify-between gap-2'>
+        <h5 className="text-2xl font-bold mb-4">{product.name}</h5>
+        <div className='flex items-center justify-between gap-2 mb-4'>
           <span className='font-black text-2xl text-redburger-500'>{formatToDollars(product.price)}</span>
-
-          <ButtonAddProduct product={product} />
         </div>
+        <ButtonProduct product={product} />
       </div>
     </div>
   )
