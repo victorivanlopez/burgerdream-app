42776683b94451cb35a42c23f5e6b58096b5863e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-01 17:17:46 -0600

    Added: decreaseQty functionality

--- src/components/order/details/BtnDecreaseQty.tsx
+++ src/components/order/details/BtnDecreaseQty.tsx
@@ -1,9 +1,20 @@
+'use client'
+import { useOrderStore } from '@/stores';
 import { MinusIcon } from '@/components/ui/icons';
+import type { Product } from '@prisma/client';
 
-export default function BtnDecreaseQty() {
+interface BtnDecreaseQtyProps {
+  idProduct: Product['id'];
+}
+
+export default function BtnDecreaseQty({ idProduct }: BtnDecreaseQtyProps) {
+
+  const decreaseQty = useOrderStore(state => state.decreaseQty);
+
   return (
     <button
       type='button'
+      onClick={() => decreaseQty(idProduct)}
     >
       <MinusIcon />
     </button>
