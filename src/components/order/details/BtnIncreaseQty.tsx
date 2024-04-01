b5a2f3f7b3c8639e4656d4306d65649a2a9b0966
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-01 17:07:02 -0600

    Added: increaseQty functionality

--- src/components/order/details/BtnIncreaseQty.tsx
+++ src/components/order/details/BtnIncreaseQty.tsx
@@ -1,9 +1,20 @@
+'use client'
 import { PlusIcon } from '@/components/ui/icons';
+import { useOrderStore } from '@/stores';
+import { Product } from '@prisma/client';
 
-export default function BtnIncreaseQty() {
+interface BtnIncreaseQtyProps {
+  idProduct: Product['id'];
+}
+
+export default function BtnIncreaseQty({ idProduct }: BtnIncreaseQtyProps) {
+
+  const increaseQty = useOrderStore(state => state.increaseQty);
+
   return (
     <button
       type='button'
+      onClick={() => increaseQty(idProduct)}
     >
       <PlusIcon />
     </button>
