351baad72bee175fd31d750c0e38cb89585eef65
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-03 13:12:13 -0600

    Created: convertToCurrency helper

---
+++ src/helpers/convertToCurrency.ts
@@ -0,0 +1,7 @@
+export const convertToCurrency = (qty: number): string => {
+  return new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+    minimumFractionDigits: 2,
+  }).format(qty);
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,1 @@
-export * from './formatToDollars';
+export * from './convertToCurrency';
