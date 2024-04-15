3ed363786757a263416dc39a211bc5b1054fa0fb
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-15 14:19:32 -0600

    Created: ToastNotification component

---
+++ src/components/ui/ToastNotification.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+export default function ToastNotification() {
+  return (
+    <ToastContainer />
+  )
+}
