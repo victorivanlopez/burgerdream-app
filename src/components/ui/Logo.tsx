8285ad980a92d0bab8df58ec80cac9197d7370d7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-17 14:30:07 -0600

    Created: Logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,14 @@
+
+export default function Logo() {
+  return (
+    <div className="flex justify-center mt-5">
+      <img
+        src="/assets/burgerdream-logo.png"
+        alt="Logotipo de BurgerDream"
+        className='w-40'
+        width={64}
+        height={64}
+      />
+    </div>
+  )
+}
