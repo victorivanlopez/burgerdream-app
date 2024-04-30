d1d1d23059a194b21459b0a6a42420fe10f2b964
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-29 21:11:38 -0600

    Added: <LinkList />

--- src/components/admin/SidebarAdmin.tsx
+++ src/components/admin/SidebarAdmin.tsx
@@ -1,17 +1,12 @@
 import { Logo } from '../ui';
-
-const adminNavigation = [
-  { url: '/admin/orders', text: 'Ordenes', blank: false },
-  { url: '/admin/products', text: 'Productos', blank: false },
-  { url: '/order/cafe', text: 'Ver Quiosco', blank: true },
-]
+import { LinkList } from './navegation';
 
 export const SidebarAdmin = () => {
   return (
     <aside className='xl:w-64 xl:h-screen bg-white shadow'>
       <Logo />
-      <nav className="flex flex-col">
-
+      <nav className="mt-10">
+        <LinkList />
       </nav>
     </aside>
   )
