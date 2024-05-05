a79fa06be9d5e27fe1b7debfe99e642a19430acb
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-04 19:25:45 -0600

    Created: OrderListAdmin component

---
+++ src/components/admin/order/OrderListAdmin.tsx
@@ -0,0 +1,21 @@
+import OrderItemAdmin from './OrderItemAdmin';
+import type { OrderWhitProducts } from '@/types';
+
+interface OrderListProps {
+  orders: OrderWhitProducts[]
+}
+
+export default function OrderListAdmin({ orders }: OrderListProps) {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6 mt-5'>
+      {
+        orders.map(order => (
+          <OrderItemAdmin
+            key={order.id}
+            order={order}
+          />
+        ))
+      }
+    </div>
+  )
+}
