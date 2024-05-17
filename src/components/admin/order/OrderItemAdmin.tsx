b7d52ebc6dedf61f4c6ee073e11770d057089533
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-17 15:21:13 -0600

    Changed: HTML structure

--- src/components/admin/order/OrderItemAdmin.tsx
+++ src/components/admin/order/OrderItemAdmin.tsx
@@ -10,12 +10,12 @@ export default function OrderItemAdmin({ order }: OrderItemAdminProps) {
   return (
     <div className="bg-white border border-gray-200 rounded-lg shadow p-8 flex flex-col">
       <h3 className="text-2xl font-bold mb-4">{order.name}</h3>
-      <p className='text-lg font-bold'>Productos ordenados: </p>
+      <p className='font-bold'>Productos ordenados: </p>
       <ul className='flex-1 py-4'>
         {
           order.products.map(product => (
-            <li 
-              key={product.orderId}
+            <li
+              key={product.id}
               className="flex items-center gap-2 py-2 border-b-2 border-gray-100 last-of-type:border-b-0">
               {product.product.name}
               <span className='font-bold text-sm'>({product.qty})</span>
@@ -24,14 +24,18 @@ export default function OrderItemAdmin({ order }: OrderItemAdminProps) {
         }
       </ul>
 
-      <div className='flex items-center justify-between py-4'>
+      <div className='flex items-center justify-between py-4 text-xl'>
         <p className='font-bold'>Total a pagar:</p>
         <span className='font-black'>{convertToCurrency(order.total)}</span>
       </div>
-      <button
-        className="py-2 px-8 cursor-pointer bg-orangeburger-400 text-black font-bold hover:bg-opacity-90 transition-colors rounded-md w-full">
-        Completar orden
-      </button>
+
+      <form>
+        <input
+          type="submit"
+          value="Completar orden"
+          className='py-2 px-8 cursor-pointer bg-orangeburger-400 text-black font-bold hover:bg-opacity-90 transition-colors rounded-md w-full'
+        />
+      </form>
     </div>
   )
 }
