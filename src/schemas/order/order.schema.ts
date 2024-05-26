559d914fe218746041f03b0a211c4f877f708783
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-25 20:03:28 -0600

    Created: OrderIdSchema

--- src/schemas/order/order.schema.ts
+++ src/schemas/order/order.schema.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
 export const OrderSchema = z.object({
-  name: z.string().min(1, 'El nombre es obligatorio'),
-  total: z.number().min(1, 'Error en el total de la orden'),
+  name: z.string()
+    .min(1, 'El nombre es obligatorio'),
+  total: z.number()
+    .min(1, 'Error en el total de la orden'),
   order: z.array(z.object({
     id: z.number(),
     name: z.string(),
@@ -11,3 +13,9 @@ export const OrderSchema = z.object({
     subtotal: z.number(),
   })),
 });
+
+export const OrderIdSchema = z.object({
+  orderId: z.string()
+    .transform(value => Number(value))
+    .refine(value => value > 0, { message: 'El valor proporcionado no es válido' }),
+})
