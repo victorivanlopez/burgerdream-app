ce61e005bc175197e3b9dba56305208fdf03893d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-25 20:03:44 -0600

    Created: completeOrder action functionality

--- src/actions/complete-order.action.ts
+++ src/actions/complete-order.action.ts
@@ -1,5 +1,28 @@
 'use server'
- 
-export async function completeOrder() {
-  console.log('Completed order');
+import { prisma } from '@/libs';
+import { OrderIdSchema } from '@/schemas';
+
+export async function completeOrder(formData: FormData) {
+  const data = {
+    orderId: formData.get('order-id'),
+  }
+
+  const result = OrderIdSchema.safeParse(data);
+
+  if (result.success) {
+    try {
+      await prisma.order.update({
+        where: {
+          id: result.data.orderId
+        },
+        data: {
+          status: true,
+          orderReadyAt: new Date(Date.now())
+        }
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 }
