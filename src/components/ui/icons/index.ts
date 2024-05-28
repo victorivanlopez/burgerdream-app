b6f7cae86f2730bb3351575a84ef43df0f8329c9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-28 17:45:56 -0600

    Created: EditIcon component

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as EditIcon } from './EditIcon';
 export { default as MinusIcon } from './MinusIcon';
 export { default as PlusIcon } from './PlusIcon';
 export { default as TrashIcon } from './TrashIcon';
