29830f44a217cfb1f4590a3ab692d6f10389a8c7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-29 15:37:06 -0600

    Added: pagination functionality

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,16 +1,26 @@
 import { ProductsListAdmin } from "@/components/admin/products";
 import { prisma } from "@/libs"
 
-async function getProducts() {
+async function getProducts(currentPage: number, pageSize: number) {
+  const skip = (currentPage - 1) * pageSize;
+
   return await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
       category: true
     }
   });
 }
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductsPage({ searchParams }: {
+  searchParams: { page: string }
+}) {
+  const currentPage = Number(searchParams.page) || 1;
+  const pageSize = 10;
+
+  const products = await getProducts(currentPage, pageSize);
+
   return (
     <>
       <ProductsListAdmin products={products} />
