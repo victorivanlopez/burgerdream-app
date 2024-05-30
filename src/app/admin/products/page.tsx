af79d418eaf615e1854c7c33c16a3e7c85a8251b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-29 19:29:36 -0600

    Added: <PaginationProducts />

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,9 @@
-import { ProductsListAdmin } from "@/components/admin/products";
-import { prisma } from "@/libs"
+import { PaginationProducts, ProductsListAdmin } from '@/components/admin/products';
+import { prisma } from '@/libs';
+
+async function getProductsCount() {
+  return await prisma.product.count();
+}
 
 async function getProducts(currentPage: number, pageSize: number) {
   const skip = (currentPage - 1) * pageSize;
@@ -19,11 +23,16 @@ export default async function ProductsPage({ searchParams }: {
   const currentPage = Number(searchParams.page) || 1;
   const pageSize = 10;
 
-  const products = await getProducts(currentPage, pageSize);
+  const productsData = getProducts(currentPage, pageSize);
+  const totalProductsData = getProductsCount();
+
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData]);
 
   return (
     <>
       <ProductsListAdmin products={products} />
+
+      <PaginationProducts currentPage={currentPage} />
     </>
   )
 }
