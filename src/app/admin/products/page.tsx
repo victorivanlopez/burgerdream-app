1f36fd459f1cafde0f1df1cda6a1d91809bf0ca3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-01 19:24:35 -0600

    Added: <SearchFormProducts />

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,6 @@
-import { PaginationProducts, ProductsListAdmin } from '@/components/admin/products';
+import { PaginationProducts, ProductsListAdmin, SearchFormProducts } from '@/components/admin/products';
 import { prisma } from '@/libs';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 async function getProductsCount() {
@@ -35,13 +36,24 @@ export default async function ProductsPage({ searchParams }: {
   if (currentPage > totalPages) redirect('/admin/products');
 
   return (
-    <>
+    <div className='px-4 sm:px-6 xl:px-8 mt-10'>
+      <div className='flex flex-col lg:flex-row lg:justify-between gap-6 mb-10'>
+        <SearchFormProducts />
+
+        <Link
+          href={'/admin/products/new'}
+          className='py-2 px-8 cursor-pointer bg-orangeburger-400 text-black font-bold hover:bg-opacity-90 transition-colors rounded-md w-full lg:w-auto text-center'
+        >
+          Agregar producto
+        </Link>
+      </div>
+
       <ProductsListAdmin products={products} />
 
       <PaginationProducts
         currentPage={currentPage}
         totalPages={totalPages}
       />
-    </>
+    </div>
   )
 }
