931de37ba7af7186230fb2f4e708bf6bbeb098c2
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-05 19:56:33 -0600

    Created: HTML structure

--- src/components/admin/products/AddProductForm.tsx
+++ src/components/admin/products/AddProductForm.tsx
@@ -1,6 +1,18 @@
+import ProductForm from "./ProductForm";
 
 export default function AddProductForm() {
   return (
-    <div>AddProductForm</div>
+    <div className="bg-white mt-10 px-5 py-10 rounded-md max-w-3xl mx-auto shadow">
+      <form
+        className="space-y-5"
+      >
+        <ProductForm />
+        <input
+          type="submit"
+          className="py-2 px-8 cursor-pointer bg-orangeburger-400 text-black font-bold hover:bg-opacity-90 transition-colors rounded-md w-full"
+          value="Crear producto"
+        />
+      </form>
+    </div>
   )
 }
--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -1,6 +1,46 @@
 
 export default function ProductForm() {
   return (
-    <div>ProductForm</div>
+    <>
+      <div className="space-y-2">
+        <label
+          className="font-bold"
+          htmlFor="name"
+        >Nombre del producto</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          className="p-2.5 text-sm border border-gray-200 bg-[#FCFDFE] focus:outline-none focus:border-orangeburger-200 w-full rounded-l-md"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label
+          className="font-bold"
+          htmlFor="price"
+        >Precio</label>
+        <input
+          id="price"
+          name="price"
+          className="p-2.5 text-sm border border-gray-200 bg-[#FCFDFE] focus:outline-none focus:border-orangeburger-200 w-full rounded-l-md"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label
+          className="font-bold"
+          htmlFor="categoryId"
+        >Categoría</label>
+        <select
+          className="p-2.5 text-sm border border-gray-200 bg-[#FCFDFE] focus:outline-none focus:border-orangeburger-200 w-full rounded-l-md"
+          id="categoryId"
+          name="categoryId"
+        >
+          <option value="">-- Seleccione --</option>
+
+        </select>
+      </div>
+    </>
   )
 }
