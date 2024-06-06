ed8016134ce0768f4bcd968474f9589e16af186f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-05 18:05:55 -0600

    Created: ProductForm component

---
+++ src/components/admin/products/ProductForm.tsx
@@ -0,0 +1,6 @@
+
+export default function ProductForm() {
+  return (
+    <div>ProductForm</div>
+  )
+}
--- src/components/admin/products/index.ts
+++ src/components/admin/products/index.ts
@@ -1,4 +1,5 @@
 export { default as AddProductForm } from './AddProductForm';
 export { default as PaginationProducts } from './PaginationProducts';
+export { default as ProductForm } from './ProductForm';
 export { default as ProductsListAdmin } from './ProductsListAdmin';
 export { default as SearchFormProducts } from './SearchFormProducts';
