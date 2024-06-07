de00f1af151cbc48fefc47ad2adb846c3969dc80
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-06 18:44:35 -0600

    Created: ProductSchema

--- src/schemas/product/product.schema.ts
+++ src/schemas/product/product.schema.ts
@@ -1,5 +1,21 @@
 import { z } from 'zod';
 
+export const ProductSchema = z.object({
+  name: z.string()
+    .trim()
+    .min(1, { message: 'El nombre del producto es obligatorio.' }),
+  price: z.string()
+    .trim()
+    .transform((value) => parseFloat(value))
+    .refine((value) => value > 0, { message: 'Precio no válido.' })
+    .or(z.number().min(1, { message: 'El precio es obligatorio.' })),
+  categoryId: z.string()
+    .trim()
+    .transform((value) => parseInt(value))
+    .refine((value) => value > 0, { message: 'La categoría es obligatoria.' })
+    .or(z.number().min(1, { message: 'La categoría es obligatoria.' })),
+})
+
 export const SearchProductsSchema = z.object({
   search: z.string()
     .trim()
