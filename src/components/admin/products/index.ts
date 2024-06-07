b939c9634e3bd9d95d762cc7dbd27a06b07f2b99
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-07 13:09:45 -0600

    Created: ImageUpload component

--- src/components/admin/products/index.ts
+++ src/components/admin/products/index.ts
@@ -1,4 +1,5 @@
 export { default as AddProductForm } from './AddProductForm';
+export { default as ImageUpload } from './ImageUpload';
 export { default as PaginationProducts } from './PaginationProducts';
 export { default as ProductForm } from './ProductForm';
 export { default as ProductsListAdmin } from './ProductsListAdmin';
