7f1bf2644f40c0761683396d0d3790c78fe35965
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-10 17:38:04 -0600

    Created: FormProductData type

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -3,3 +3,10 @@ import { Category, Product } from '@prisma/client';
 export type ProductWithCategories = Product & {
   category: Category
 }
+
+export type FormProductData = {
+  name: FormDataEntryValue | null;
+  price: number;
+  categoryId: number;
+  image: string;
+}
