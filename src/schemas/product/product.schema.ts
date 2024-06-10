80d3ffbe44b66ee4c9ad4df5c7da2313c2eca692
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-10 17:38:59 -0600

    Created: ProductSchema

--- src/schemas/product/product.schema.ts
+++ src/schemas/product/product.schema.ts
@@ -1,19 +1,25 @@
 import { z } from 'zod';
 
 export const ProductSchema = z.object({
-  name: z.string()
-    .trim()
-    .min(1, { message: 'El nombre del producto es obligatorio.' }),
-  price: z.string()
-    .trim()
-    .transform((value) => parseFloat(value))
-    .refine((value) => value > 0, { message: 'Precio no válido.' })
-    .or(z.number().min(1, { message: 'El precio es obligatorio.' })),
-  categoryId: z.string()
-    .trim()
-    .transform((value) => parseInt(value))
-    .refine((value) => value > 0, { message: 'La categoría es obligatoria.' })
-    .or(z.number().min(1, { message: 'La categoría es obligatoria.' })),
+  name:
+    z.string()
+      .trim()
+      .min(1, { message: 'El nombre del producto es obligatorio.' }),
+  price:
+    z.string()
+      .trim()
+      .transform((value) => parseFloat(value))
+      .refine((value) => value > 0, { message: 'Precio no válido.' })
+      .or(z.number().min(1, { message: 'El precio es obligatorio.' })),
+  categoryId:
+    z.string()
+      .trim()
+      .transform((value) => parseInt(value))
+      .refine((value) => value > 0, { message: 'La categoría es obligatoria.' })
+      .or(z.number().min(1, { message: 'La categoría es obligatoria.' })),
+  image:
+    z.string()
+      .min(1, { message: 'La imagen es obligatoria.' }),
 })
 
 export const SearchProductsSchema = z.object({
