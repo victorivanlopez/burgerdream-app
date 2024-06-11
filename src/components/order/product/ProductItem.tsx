df6f93478500261e7f40efcad94a37e52d9a0164
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-11 12:43:41 -0600

    Changed: image product functionality

--- src/components/order/product/ProductItem.tsx
+++ src/components/order/product/ProductItem.tsx
@@ -1,5 +1,5 @@
 import { ButtonProduct } from '.';
-import { convertToCurrency } from '@/helpers';
+import { convertToCurrency, getImagePath } from '@/helpers';
 import type { Product } from '@prisma/client';
 
 interface ProductItemProps {
@@ -8,9 +8,11 @@ interface ProductItemProps {
 
 export default function ProductItem({ product }: ProductItemProps) {
 
+  const imagePath = getImagePath(product.image);
+
   return (
     <div className=" bg-white border border-gray-200 rounded-lg shadow md:flex-row">
-      <img className="object-cover rounded-t-lg h-44 w-full md:rounded-none md:rounded-s-lg" src={`/assets/products/${product.image}.webp`} alt={`Imagenen del producto ${product.name}`} />
+      <img className="object-cover rounded-t-lg h-44 w-full md:rounded-none md:rounded-s-lg" src={imagePath} alt={`Imagen del producto ${product.name}`} />
       <div className="p-4">
         <h5 className="text-2xl font-bold mb-4">{product.name}</h5>
         <div className='flex items-center justify-between gap-2 mb-4'>
