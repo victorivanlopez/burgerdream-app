dffc401ec9e0cd9f73457f56b0f91392f8c226d2
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-22 20:38:27 -0600

    Created: getProductById functionality

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,5 +1,34 @@
+import { prisma } from '@/libs';
+import { notFound } from 'next/navigation';
 
-export default function EditProductPage() {
+async function getProductById(id: number) {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id
+      },
+      include: {
+        category: true
+      }
+    });
+
+    if (!product) undefined;
+
+    return product;
+
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error en el servidor');
+  }
+}
+
+export default async function EditProductPage({ params }: { params: { id: string } }) {
+  const product = await getProductById(Number(params.id));
+
+  if (!product) {
+    notFound();
+  }
+
   return (
     <div>EditProductPage</div>
   )
