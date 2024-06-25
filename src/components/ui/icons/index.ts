7b83ac04df8563c9c424dd1aa25d4411ce882a93
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-25 17:10:55 -0600

    Created: ArrowLeftIcon

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as ArrowLeftIcon } from './ArrowLeftIcon';
 export { default as EditIcon } from './EditIcon';
 export { default as MinusIcon } from './MinusIcon';
 export { default as PlusIcon } from './PlusIcon';
