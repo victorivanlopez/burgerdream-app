499dd7b5b40b05d97c30332ba5765d3d2f399c24
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-06-29 21:29:08 -0600

    Created: refreshOrders functionality

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,4 +1,5 @@
 
+import { revalidatePath } from 'next/cache';
 import { OrderListAdmin } from '@/components/admin/order';
 import { prisma } from '@/libs';
 
@@ -17,15 +18,33 @@ async function getPendingOrders() {
   })
 }
 
+const refreshOrders = async () => {
+  'use server'
+  revalidatePath('/admin/orders');
+}
+
 export default async function OrdersPage() {
   const orders = await getPendingOrders();
   return (
     <>
-      {
-        orders.length === 0
-          ? <p>No hay ordenes pendientes.</p>
-          : <OrderListAdmin orders={orders} />
-      }
+      <h1 className="text-xl font-bold mb-5">Administrar ordenes</h1>
+      <form
+        action={refreshOrders}
+      >
+        <input
+          type="submit"
+          value="Actualizar ordenes"
+          className='py-2 px-8 cursor-pointer bg-orangeburger-400 text-black font-bold hover:bg-opacity-90 transition-colors rounded-md w-full lg:w-auto text-center'
+        />
+      </form>
+
+      <div className='mt-5'>
+        {
+          orders.length === 0
+            ? <p>No hay ordenes pendientes.</p>
+            : <OrderListAdmin orders={orders} />
+        }
+      </div>
     </>
   )
 }
