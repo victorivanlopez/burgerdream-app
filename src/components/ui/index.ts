9f03cc55014f61247c1c8330eda439bcc3667494
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-01 11:46:58 -0600

    Created: Spinner component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as GoBackButton } from './GoBackButton';
 export { default as Logo } from './Logo';
+export { default as Spinner } from './Spinner';
 export { default as ToastNotification } from './ToastNotification';
