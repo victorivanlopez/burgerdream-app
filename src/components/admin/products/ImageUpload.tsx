c82fe11fcfaded676ede1a8e3ad354f77cd1c9b0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-06 14:18:33 -0600

    Added: <CloudUpIcon />

--- src/components/admin/products/ImageUpload.tsx
+++ src/components/admin/products/ImageUpload.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { getImagePath } from '@/helpers';
-import { CldUploadWidget } from 'next-cloudinary';
 import { useEffect, useState } from 'react';
+import { CldUploadWidget } from 'next-cloudinary';
+import { CloudUpIcon } from '@/components/ui/icons';
+import { getImagePath } from '@/helpers';
 
 export default function ImageUpload({ image }: { image: string | undefined }) {
   const [imageUrl, setImageUrl] = useState('');
@@ -42,9 +43,7 @@ export default function ImageUpload({ image }: { image: string | undefined }) {
                   : (
                     <label className="flex flex-col items-center justify-center cursor-pointer">
                       <div className="flex flex-col items-center justify-center pt-5 pb-6 text-gray-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
-                        </svg>
+                        <CloudUpIcon />
                         <p className="mb-2 text-sm">Agregar imagen</p>
                       </div>
                     </label>
