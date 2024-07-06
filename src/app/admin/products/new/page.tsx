875f455ed1c7d8c47c2430822afec5af4c48a834
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-06 14:20:08 -0600

    Added: <GoBackButton />

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,9 +1,12 @@
 import { AddProductForm, ProductForm } from '@/components/admin/products';
+import { GoBackButton } from '@/components/ui';
 
 export default function NewProductPage() {
   return (
     <>
       <h1 className="text-xl font-bold">Nuevo Producto</h1>
+
+      <GoBackButton />
 
       <AddProductForm>
         <ProductForm />
