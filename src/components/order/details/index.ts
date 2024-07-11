456ddae36b0597effc0a9d3d432f4311fc049243
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-10 18:42:45 -0600

    Changed: root component

--- src/components/order/details/index.ts
+++ src/components/order/details/index.ts
@@ -1,8 +1,5 @@
-export { default as BtnDecreaseQty } from './BtnDecreaseQty';
-export { default as BtnDeleteProductOrder } from './BtnDeleteProductOrder';
-export { default as BtnIncreaseQty } from './BtnIncreaseQty';
+export { default as FormDetails } from './FormDetails';
 export { default as MainContainerDetails } from './MainContainerDetails';
 export { default as OrderTotalDetails } from './OrderTotalDetails';
 export { default as ProductItemDetails } from './ProductItemDetails';
 export { default as ProductListDetails } from './ProductListDetails';
-export { default as FormDetails } from './FormDetails';
