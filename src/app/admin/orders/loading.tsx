9ae687e78480f7e9a7aa822184e47bf698976328
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-16 14:24:11 -0600

    Created: loading component

---
+++ src/app/admin/orders/loading.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from '@/components/ui';
+
+export default function OrdersLoading() {
+  return <Spinner />
+}
---
+++ src/app/admin/products/loading.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from '@/components/ui';
+
+export default function ProductsLoading() {
+  return <Spinner />
+}
