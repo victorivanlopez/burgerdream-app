6886496d243e8945c17741b2f3ec59f67b34eddf
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-17 18:48:54 -0600

    Updated: styles

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -13,12 +13,12 @@ export default function OrdersPage() {
   if (isLoading) return <Spinner />
 
   if (orders) return (
-    <div className='p-2 md:p-4 lg:p-6'>
-      <h1 className="text-center mt-20 text-3xl lg:text-6xl font-black">Ordenes listas</h1>
-
+    <div className='px-2 md:px-6 py-4 lg:py-8'>
       <Logo />
 
-      <div className='mt-10'>
+      <h1 className="text-center mt-5 text-4xl lg:text-6xl font-black">Ordenes listas</h1>
+
+      <div className='mt-5 md:mt-10'>
         {
           orders.length === 0
             ? <p className='text-center'>No hay ordenes pendientes.</p>
